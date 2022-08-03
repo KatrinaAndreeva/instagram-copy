a3f1b82357f2531c310705868660aef3a395628e
Kate <katya-andreeva@ukt.net>
2022-08-03 19:06:21 +0300

    "newsfeed component"

--- components/Feed.js
+++ components/Feed.js
@@ -3,14 +3,14 @@ import Stories from './Stories';
 
 export default function Feed() {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 mx-auto md:grid-cols-3 md:max-w-6xl">
+      <section className="md:col-span-2">
         {/* stories */}
         <Stories />
         <Posts />
         {/* posts */}
       </section>
-      <section>
+      <section className="hidden md:col-span-1 md:inline-grid ">
         {/* mini profiles */}
 
         {/* suggesctions */}
--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,50 @@
+import {
+  DotsHorizontalIcon,
+  HeartIcon,
+  ChatIcon,
+  BookmarkIcon,
+  EmojiHappyIcon,
+} from '@heroicons/react/outline';
+
 export default function Post({ img, userImg, caption, username, id }) {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white border rounded-md my-7">
+      {/* POst header */}
+      <div className="flex items-center p-5">
+        <img
+          className="object-cover h-12 p-1 mr-3 border rounded-full"
+          src={userImg}
+          alt={username}
+        />
+        <p className="flex-1 font-bold ">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+      {/* image post */}
+      <img className="object-cover w-full" src={img} alt={caption} />
+      {/* btn  */}
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
+      {/* post comments */}
+      <p className="p-5 truncate ">
+        <span className="mr-2 font-bold">{username}</span>
+        {caption}
+      </p>
+
+      {/* input comments */}
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          className="flex-1 border-none focus:ring-0"
+          type="text"
+          placeholder="Enter your comment..."
+        />
+        <button className="text-blue-400 text-bold">Post</button>
+      </form>
     </div>
   );
 }
