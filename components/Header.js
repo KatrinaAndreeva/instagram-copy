67a33d6749950c6c46526e112e52bb20c7c221ec
Kate <katya-andreeva@ukt.net>
2022-08-10 12:29:41 +0300

    "added comments to firebase"

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,12 @@ import { HomeIcon } from '@heroicons/react/solid';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atom/modalAtom';
+import { useRouter } from 'next/router';
 
 export default function Header() {
   const { data: session } = useSession();
   const [open, setOpen] = useRecoilState(modalState);
+  const router = useRouter();
 
   return (
     <div className="sticky top-0 z-30 bg-white border-b shadow-sm">
@@ -15,6 +17,7 @@ export default function Header() {
         {/* logo */}
         <div className="relative hidden w-24 h-24 cursor-pointer lg:inline-grid">
           <Image
+            onClick={() => router.push('/')}
             layout="fill"
             className="object-contain "
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
@@ -24,6 +27,7 @@ export default function Header() {
         </div>
         <div className="relative inline-grid w-10 h-24 cursor-pointer lg:hidden">
           <Image
+            onClick={() => router.push('/')}
             layout="fill"
             className="object-contain "
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"
@@ -43,7 +47,10 @@ export default function Header() {
         </div>
         {/* auth section */}
         <div className="flex items-center space-x-4 cursor-pointer">
-          <HomeIcon className="hidden h-6 transition-transform duration-200 ease-out cursor-pointer hover:scale-125 md:inline-flex" />
+          <HomeIcon
+            onClick={() => router.push('/')}
+            className="hidden h-6 transition-transform duration-200 ease-out cursor-pointer hover:scale-125 md:inline-flex"
+          />
           {session ? (
             <>
               <PlusCircleIcon
--- components/Post.js
+++ components/Post.js
@@ -5,8 +5,26 @@ import {
   BookmarkIcon,
   EmojiHappyIcon,
 } from '@heroicons/react/outline';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export default function Post({ img, userImg, caption, username, id }) {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState('');
+  async function sendComment(e) {
+    e.preventDefault();
+    const commentToSend = comment;
+    setComment('');
+    await addDoc(collection(db, 'posts', id, 'comments'), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  }
+
   return (
     <div className="bg-white border rounded-md my-7">
       {/* POst header */}
@@ -21,30 +39,46 @@ export default function Post({ img, userImg, caption, username, id }) {
       </div>
       {/* image post */}
       <img className="object-cover w-full" src={img} alt={caption} />
+
       {/* btn  */}
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4">
-          <HeartIcon className="btn" />
-          <ChatIcon className="btn" />
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            <HeartIcon className="btn" />
+            <ChatIcon className="btn" />
+          </div>
+          <BookmarkIcon className="btn" />
         </div>
-        <BookmarkIcon className="btn" />
-      </div>
+      )}
+
       {/* post comments */}
+
       <p className="p-5 truncate ">
         <span className="mr-2 font-bold">{username}</span>
         {caption}
       </p>
 
       {/* input comments */}
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="h-7" />
-        <input
-          className="flex-1 border-none focus:ring-0"
-          type="text"
-          placeholder="Enter your comment..."
-        />
-        <button className="text-blue-400 text-bold">Post</button>
-      </form>
+      {session && (
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7" />
+          <input
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            className="flex-1 border-none focus:ring-0"
+            type="text"
+            placeholder="Enter your comment..."
+          />
+          <button
+            type="submit"
+            onClick={sendComment}
+            disabled={!comment.trim()}
+            className="text-blue-400 text-bold disabled:text-blue-200"
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 }
